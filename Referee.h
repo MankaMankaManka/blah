e1f119ab9504fe5238a6e04de81a1b34fc396169
MankaMankaManka <109943723+MankaMankaManka@users.noreply.github.com>
2023-03-07 11:28:10 +1030

    Add files via upload

---
+++ Referee.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Player.h"
+#ifndef REFEREE_H
+#define REFEREE_H
+
+class Referee
+{
+	public:
+	Referee();
+	Player*refGame(Player*player1, Player*player2);
+	char m1;
+	char m2;
+};
+
+#endif
